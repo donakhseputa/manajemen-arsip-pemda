66d1db6539b049545a40d195e8c1b7d8a9b83f7c
Donny Akhmad Septa Utama <donny.akhmad.septa.utama26@gmail.com>
2026-05-20 05:50:35 +0700

    feat: enable timezone and locale from .env

---
+++ app/Http/Controllers/ArchiveClassificationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ArchiveClassification;
+use App\Http\Requests\StoreArchiveClassificationRequest;
+use App\Http\Requests\UpdateArchiveClassificationRequest;
+
+class ArchiveClassificationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreArchiveClassificationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreArchiveClassificationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ArchiveClassification  $archiveClassification
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ArchiveClassification $archiveClassification)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ArchiveClassification  $archiveClassification
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ArchiveClassification $archiveClassification)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateArchiveClassificationRequest  $request
+     * @param  \App\Models\ArchiveClassification  $archiveClassification
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateArchiveClassificationRequest $request, ArchiveClassification $archiveClassification)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ArchiveClassification  $archiveClassification
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ArchiveClassification $archiveClassification)
+    {
+        //
+    }
+}
